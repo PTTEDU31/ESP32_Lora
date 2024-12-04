b8fc11957bddbd31aa50515a1e200055abe5ddfc
PTT3108 <tung310803vn@gmail.com>
2024-12-04 17:02:06 +0700

    update

--- lib/Lora_Mesher/src/services/WifiService.cpp
+++ lib/Lora_Mesher/src/services/WifiService.cpp
@@ -1,13 +1,20 @@
 #include "WiFiService.h"
 #include "esp_mac.h"
+#include "WiFi.h"
 void WiFiService::init()
 {
     uint8_t baseMac[6];
-    // Get MAC address for WiFi station
-    esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-    char baseMacChr[18] = {0};
-    sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-    localAddress = (baseMac[4] << 8) | baseMac[5]; 
+// Get MAC address for WiFi station
+// esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+// char baseMacChr[18] = {0};
+// sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+// localAddress = (baseMac[4] << 8) | baseMac[5];
+#ifdef ARDUINO
+    WiFi.macAddress(baseMac);
+#else
+    efuse_hal_get_mac(baseMac);
+#endif
+    localAddress = (baseMac[4] << 8) | baseMac[5];
     ESP_LOGI(LM_TAG, "Local LoRa address (from WiFi MAC): %X", localAddress);
 }
 
